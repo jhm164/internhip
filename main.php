48e343489c28300f2e61759213a16f349260f7c7
saurabh-solkar16 <saurabhsolkar4@gmail.com.com>
2018-10-15 07:01:22 +0530

    second commit

--- main.php
+++ main.php
@@ -15,6 +15,9 @@
   	body{
   		background-color: #67696b;
   	}
+  	p{
+  		float: left;
+  	}
   	
 .table tbody tr td {
   background-color: white;
@@ -46,23 +49,20 @@ app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.myWelcomes = response.data;
     });
   }
-
-}]);
-var app = angular.module('myApp', []);
-app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
-  $scope.clickappmode = function(msg) {
+   $scope.clickappmode = function(msg) {
     $http.get("sqlfile.php?appmode="+msg) .then(function(response) {
         $scope.myWelcomes = response.data;
     });
   }
 
 }]);
+
 </script>
 </head>
 <body class="container-fluid" ng-app="myApp" ng-controller="myCtrl" >
 <div ng-click="clickButton('ARW')"> ARW</div>
 <div ng-click="clickappmode('on campus')"> cdw</div>
-<center style=" margin-top: 100px;  box-shadow: 7px 7px 7px 7px black;">
+<div style=" margin-top: 100px;  box-shadow: 7px 7px 7px 7px black;">
 <div class="panel panel-default">
   <div class="panel-heading" style="background-color:#a1a7ad;">
   	<center>
@@ -73,12 +73,12 @@ app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
   	<div class="col-lg-3 col-md-3" style="background-color: #edeeef;padding-left:  15px;padding-right: 15px;padding-bottom:  15px;">
 
   <p style="float: right;padding-top:  15px;"><a href="">apply_filter</a>
-  </p>
+  </h5>
 <div style="float: left;padding-top:  15px;"><h4>Organisation:</h4></div>
 	<select class="form-control form-control-sm">
 <option>--select--</option>
-  <option id="ARW"><div ng-click="clickButton('ARW')"> ARW</div></option>
-  <option ><div ng-click="clickButton('dsdadsa')"> cdw</div></option>
+  <option  ng-click="clickButton('ARW')"><div ng-click="clickButton('ARW')"> ARW</div></option>
+  <option ng-click="clickButton('dsdadsa')" ><div ng-click="clickButton('dsdadsa')"> cdw</div></option>
 </select>	
 <div style="float: left;padding-top:  15px;"><h4>Engineering Stream:</h4></div>
 	<select class="form-control form-control-sm">
@@ -97,15 +97,24 @@ app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
 
   	</div>
   	<div class="col-lg-1 col-md-1"></div>
-  	<div  class="col-lg-8 col-md-8" >
+  	<div  class="col-lg-8 col-md-8" style="overflow: scroll;">
   		<h1>Interview Experiances:ARW</h1>
 
-<div ng-Repeat="myWelcome in myWelcomes" style="border: 1px solid black;margin: 2px;">
-	{{myWelcome.name}}
+<div ng-Repeat="myWelcome in myWelcomes" style="border: 1px solid black;margin: 2px;padding: 12px;float: left; width: 100%; padding-left:15px; ">
+	<h5>Job Location : <b> {{myWelcome.location}}</b></h5>
+	<h5>Oganisation : <b> {{myWelcome.organisation}}</b></h5>
+	<h5>Application Mode : <b> {{myWelcome.applimode}}</b></h5>
+	<h5>Job Location : <b> {{myWelcome.location}}</b></h5>
+	<h5>Selection Procedure: <b> {{myWelcome.selectionpro}}</b></h5>
+	<h5>Technical Interview: <b> {{myWelcome.technical}}</b></h5>
+	<h5>Analytical Question: <b> {{myWelcome.analyticalq}}</b></h5>
+	<h5>HR Question: <b> {{myWelcome.hrq}}</b></h5>
+	<h5>Suggesions: <b> {{myWelcome.suggession}}</b></h5>
+	<h5>Shared By: <b> {{myWelcome.name}}</b></h5>
 </div>
   </div>
 </div>
-</center>
+</div>
 
 </body>
 </html>
